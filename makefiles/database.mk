e967d06b49a769c206c45529b6c31e445a7e0937
Florian Abélard <f.abelard@gmail.com>
2020-09-04 15:50:33 +0200

    configure database environment

--- makefiles/database.mk
+++ makefiles/database.mk
@@ -3,15 +3,25 @@
 #------------------------------------------------------------------------------
 
 database-admin-exec = docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T --user root db ${1}
+database-doctrine-exec = docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T --user ${USER_ID} php ${1}
 
 #------------------------------------------------------------------------------
 
-db-init: ##@database create and populate database
+db-init: db-create-database db-update-create ##@database create and populate database
 
+db-create-database: clean-db
+	$(call database-doctrine-exec, php bin/console doctrine:database:create)
+
+db-update-create: 
+	$(call database-doctrine-exec, php bin/console doctrine:schema:create)
+
+db-update-schema: 
+	$(call database-doctrine-exec, php bin/console doctrine:schema:update --force)
 
 #------------------------------------------------------------------------------
 
 clean-db: ##@database clean database
+	$(call database-doctrine-exec, php bin/console doctrine:database:drop --force)
 
 #------------------------------------------------------------------------------
 
